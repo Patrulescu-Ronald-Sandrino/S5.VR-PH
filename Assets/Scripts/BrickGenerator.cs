0039a8ac91ce38af8c128da5697c07b39d9749b1
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-01-08 11:35:36 +0200

    add B to recreate bricks wall & README.md

--- Assets/Scripts/BrickGenerator.cs
+++ Assets/Scripts/BrickGenerator.cs
@@ -1,10 +1,17 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 // unity create wall of bricks https://youtu.be/cCdx-MC7XiQ
 public class BrickGenerator : MonoBehaviour
 {
-    public int rows = 10;
-    public int columns = 10;
+    public const int rows = 10;
+    public  int columns = 10;
+
+    private List<GameObject> _bricks = new();
+    private bool _enabled = true;
+    
     void Start()
     {
         var rainbow = new Color[] { Color.red, Color.yellow, Color.green, Color.blue, Color.magenta };
@@ -19,10 +26,31 @@ public class BrickGenerator : MonoBehaviour
             {
                 // var brick = Instantiate(prefab, new Vector3(-10, 0.5f + row, z), Quaternion.identity);
                 var brick = Instantiate(prefab, new Vector3(position.x, position.y + row, position.z - z), Quaternion.identity);
+                _bricks.Add(brick);
                 brick.GetComponent<Renderer>().material.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
                 // brick.GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(Mathf.PingPong(Time.time, 1), (float)z/rows, (float)row/rows));
                 brick.GetComponent<Renderer>().material.SetColor("_Color", rainbow[(int)((float)(row + 1)/rows * (rainbow.Length - 1.0f))]);
             }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            Debug.Log("B pressed");
+            if (_enabled)
+            {
+                foreach (var brick in _bricks)
+                {
+                    Destroy(brick);
+                }
+            }
+            else
+            {
+                Start();
+            }
+            _enabled = !_enabled;
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# PH
+
+
+keys:
+arrows to move cannon
+left mouse button to shoot
+B to reset wall
